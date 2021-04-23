c684b2d2f52c432ae6be6d697be25f57e3f63b28
Steve Frenzel <stevefrenzel@gmx.de>
2021-04-23 08:04:30 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,55 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com;
+  child-src *.youtube.com *.youtube-nocookie.com;
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+  font-src 'self';
+`;
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
+];
+
+module.exports = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      { source: '/', headers: securityHeaders },
+      { source: '/:path*', headers: securityHeaders },
+    ];
+  },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+};
