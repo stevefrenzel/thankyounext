8d6a3a2ed516ed49291b2141293c47619b837ce8
Steve Frenzel <stevefrenzel@gmx.de>
2021-12-09 18:08:42 -0600

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Footer: NextPage = () => {
+  return (
+    <footer>
+      <p>Made by Steve Frenzel</p>
+    </footer>
+  )
+}
+
+export default Footer
