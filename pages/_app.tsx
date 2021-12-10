8b87ccaed9e14e57fb9ca960810ee45c89882c2a
Steve Frenzel <stevefrenzel@gmx.de>
2021-12-09 18:08:10 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,8 @@
-import '@styles/reset.scss';
-import '@styles/globals.scss';
-import '@styles/delete-me.scss';
-import { AppProps } from 'next/app';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-// Delete this if runtime JavaScript is needed:
-export const config = {
-  unstable_runtimeJS: false,
-};
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
+
+export default MyApp
