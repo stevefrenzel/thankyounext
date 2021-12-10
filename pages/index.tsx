a51f8698a5a0490c2526f560f9865bf546f02012
Steve Frenzel <stevefrenzel@gmx.de>
2021-12-09 18:08:12 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,39 @@
-import Meta from '@utils/Meta';
-import DeleteMe from '@components/DeleteMe';
+import type { NextPage } from 'next'
+import Meta from 'components/Meta'
+import SkipLink from 'components/SkipLink'
+import Footer from 'components/Footer'
 
-// Delete this if runtime JavaScript is needed:
-export const config = {
-  unstable_runtimeJS: false,
-};
-
-export default function Home() {
+const Home: NextPage = () => {
   return (
-    <>
-      <Meta />
-      <a href="#main">Skip to main content</a>
-      <DeleteMe />
-    </>
-  );
+    <div className="font-serif h-screen px-4 flex flex-col justify-center items-center max-w-3xl mx-auto">
+      {/* <Meta /> */}
+      {/* <SkipLink content="Skip to main content" /> */}
+      <h1 className="text-6xl font-bold">ThankYouNext 🖤</h1>
+      <br />
+      <p className="text-4xl">
+        A highly performant and accessible Next.js starter template
+      </p>
+      <br />
+      <main id="main" className="text-4xl">
+        <p>
+          This Next.js starter template is meant to be easy to use, fun to work
+          with and very performant. It was bootstrapped with create-next-app and
+          has the following features:
+        </p>
+        <br />
+        <ul className="list-disc list-inside" role="list">
+          <li>TypeScript</li>
+          <li>Tailwind CSS</li>
+          <li>Preact</li>
+          <li>Ready-to-use SEO setup</li>
+          <li>Security headers</li>
+          <li>Prettier config</li>
+          <li>Dark mode</li>
+        </ul>
+      </main>
+      <Footer />
+    </div>
+  )
 }
+
+export default Home
