60d92097853526e83cf607e99822f404d5082051
Steve Frenzel <stevefrenzel@gmx.de>
2021-12-11 07:29:21 -0600

    Update SkipLink.tsx

--- components/SkipLink.tsx
+++ components/SkipLink.tsx
@@ -8,7 +8,20 @@ const SkipLink: NextPage<Props> = ({ content }) => {
   return (
     <a
       href="#main"
-      className="inline-block absolute bg-blue-600 text-white text-xl p-6 mt-2 ml-2 rounded-md hover:underline focus:underline"
+      className="
+        absolute
+        top-0
+        right-full
+        mt-2
+        p-2
+        active:underline
+        active:bg-blue-500
+        active:text-white
+        active:right-auto
+        focus:underline
+        focus:bg-blue-500
+        focus:text-white
+        focus:right-auto"
     >
       {content}
     </a>
