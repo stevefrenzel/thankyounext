6eb82cde3f3442db325f4b0c731f67f86027b77b
Steve Frenzel <stevefrenzel@gmx.de>
2021-12-11 07:29:32 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// Delete this if runtime JavaScript is needed:
+export const config = {
+  unstable_runtimeJS: false,
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
